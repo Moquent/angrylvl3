2a92bbdaa7b2442d1a9ea2f92bde4a7c98490211
TejeshCodes <tejeshreddy907@gmail.com>
2020-04-30 19:44:52 +0530

    Commit

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var obj;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,7 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    obj = new Ground(150, 305, 300, 170);
 }
 
 function draw(){
@@ -58,5 +59,7 @@ function draw(){
     log4.display();
     log5.display();
 
+    obj.display();
+    
     bird.display();
 }
